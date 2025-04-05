d51274637a7033828dd5c2d3992b0d2941667bad
dromeda2004 <117053370+dromeda2004@users.noreply.github.com>
2025-04-04 19:18:33 -0500

    Update serviceTest.py

--- serviceTest.py
+++ serviceTest.py
@@ -1,36 +1,52 @@
-from service import Service
+from unittest import TestCase
+from main import Service
 
-#Successful
-def test_add_service1():
-    service = Service()
-    assert service.add_service("Hair Perm", "A permanent wave, commonly called a perm or permanent, is a hairstyle consisting of waves or curls set into the hair. The curls may last a number of months, hence the name.", "60.00")
+class TestService(TestCase):
+    def test_add_service1(self):
+        service = Service()
+        assert service.add_service("Hair Perm",
+                                   "A permanent wave, commonly called a perm or permanent, is a hairstyle consisting of waves or curls set into the hair. The curls may last a number of months, hence the name.",
+                                   "60.00") == "Service added successfully"
 
-#Invalid price
-def test_add_service2():
-    service = Service()
-    assert service.add_service("Hair Perm", "A permanent wave, commonly called a perm or permanent, is a hairstyle consisting of waves or curls set into the hair. The curls may last a number of months, hence the name.", "60/00")
+    # Invalid cost
+    def test_add_service2(self):
+        service = Service()
+        assert service.add_service("Hair Perm",
+                                   "A permanent wave, commonly called a perm or permanent, is a hairstyle consisting of waves or curls set into the hair. The curls may last a number of months, hence the name.",
+                                   "60/00") == "Error: Invalid cost"
 
-#Exceptional price
-def test_add_service3():
-    service = Service()
-    assert service.add_service("Hair Perm", "A permanent wave, commonly called a perm or permanent, is a hairstyle consisting of waves or curls set into the hair. The curls may last a number of months, hence the name.", "abcd")
+    # Exceptional cost
+    def test_add_service3(self):
+        service = Service()
+        assert service.add_service("Hair Perm",
+                                   "A permanent wave, commonly called a perm or permanent, is a hairstyle consisting of waves or curls set into the hair. The curls may last a number of months, hence the name.",
+                                   "abcd") == "Error: Invalid cost"
 
-#Invalid service description
-def test_add_service4():
-    service = Service()
-    assert service.add_service("Hair Perm", "A permanent wave, commonly called a perm or permanent, is a hairstyle consisting of waves or curls set into the hair. The curls may last a number of months, hence the name.A permanent wave, commonly called a perm or permanent, is a hairstyle consisting of waves or curls set into the hair. The curls may last a number of months, hence the name." , "60.00")
+    # Invalid service description
+    def test_add_service4(self):
+        service = Service()
+        assert service.add_service("Hair Perm",
+                                   "A permanent wave, commonly called a perm or permanent, is a hairstyle consisting of waves or curls set into the hair. The curls may last a number of months, hence the name. A permanent wave, commonly called a perm or permanent, is a hairstyle consisting of waves or curls set into the hair. The curls may last a number of months, hence the name. A permanent wave, commonly called a perm or permanent, is a hairstyle consisting of waves or curls set into the hair. The curls may last a number of months, hence the name.",
+                                   "60.00") == "Error: Invalid description"
 
-#Exceptional service description
-def test_add_service7():
-    service = Service()
-    assert service.add_service("Hair Perm", "A \\ permanent wave, ~commonly called a perm or permanent, is a hairstyle consisting of waves or curls set into the hair. The curls may last a number of months, hence the name.", "60.00")
+    # Exceptional service description
+    def test_add_service7(self):
+        service = Service()
+        assert service.add_service("Hair Perm",
+                                   "A \\ permanent wave, ~commonly called a perm or permanent, is a hairstyle consisting of waves or curls set into the hair. The curls may last a number of months, hence the name.",
+                                   "60.00") == "Error: Invalid description"
 
-#Invalid service name
-def test_add_service10():
-    service = Service()
-    assert service.add_service("A permanent wave, commonly called a perm or permanent, is a hairstyle consisting of waves or curls set into the hair. The curls may last a number of months, hence the name.", "A permanent wave, commonly called a perm or permanent, is a hairstyle consisting of waves or curls set into the hair. The curls may last a number of months, hence the name.", "60.00")
+    # Invalid service name
+    def test_add_service10(self):
+        service = Service()
+        assert service.add_service(
+            "A permanent wave, commonly called a perm or permanent, is a hairstyle consisting of waves or curls set into the hair. The curls may last a number of months, hence the name.",
+            "A permanent wave, commonly called a perm or permanent, is a hairstyle consisting of waves or curls set into the hair. The curls may last a number of months, hence the name.",
+            "60.00") == "Error: Invalid name"
 
-#Exceptional service name
-def test_add_service19():
-    service = Service()
-    assert service.add_service("Hair \\ Perm", "A permanent wave, commonly called a perm or permanent, is a hairstyle consisting of waves or curls set into the hair. The curls may last a number of months, hence the name.", "60.00")
+    # Exceptional service name
+    def test_add_service19(self):
+        service = Service()
+        assert service.add_service("",
+                                   "A permanent wave, commonly called a perm or permanent, is a hairstyle consisting of waves or curls set into the hair. The curls may last a number of months, hence the name.",
+                                   "60.00") == "Exceptional: No input"

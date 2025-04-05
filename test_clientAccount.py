4c304e78d54ce6b690e37c8b0eb7484dd02c1ce0
Kevin Le <kevinle.2088@gmail.com>
2025-04-04 19:58:14 -0500

    Update test_clientAccount.py
    Final update! All test cases should be there!

--- test_clientAccount.py
+++ test_clientAccount.py
@@ -71,36 +71,43 @@ def test_firstName14():
     testFname = ClientAccount()
     assert testFname.register_user("john_doe", "Password123!", "john@example.com", "john", "doe!@#") == "Error: last name contains invalid characters."
 
-
 #LOGIN TESTING
 
-def test_login1(): #successful login
+def test_login15(): #successful login
     login = ClientAccount()
     login.register_user("john_doe", "Password123!", "john@example.com", "john", "doe") 
     assert login.login_user("john_doe", "Password123!") == "User john_doe logged in successfully."
 
-def test_login2(): #error login : Valid Username , Invalid Password
+def test_login16(): #error login : Valid Username , Invalid Password
     login = ClientAccount()
     login.register_user("john_doe", "Password123!", "john@example.com", "john", "doe") 
     assert login.login_user("john_doe", "Password") == "Error: Invalid username or password."
 
-def test_login3(): #error login: Valid Username , Exception Passowrd
+def test_login17(): #error login: Valid Username , Exception Passowrd
     login = ClientAccount()
     login.register_user("john_doe", "Password123!", "john@example.com", "john", "doe") 
     assert login.login_user("john_doe", "Password 1") == "Error: Invalid username or password."
 
+def test_login18(): #error login: Invalid Username , Exception Passowrd
+    login = ClientAccount()
+    login.register_user("john_doe", "Password123!", "john@example.com", "john", "doe") 
+    assert login.login_user("john", "Password 1") == "Error: Invalid username or password."
 
+def test_login19(): #error login: Exception Username , Exception Passowrd
+    login = ClientAccount()
+    login.register_user("john_doe", "Password123!", "john@example.com", "john", "doe") 
+    assert login.login_user("", "Password 1") == "Error: Invalid username or password."
 
 #LOGOUT TESTING
 
-def test_logout1():
+def test_logout20():
     logout = ClientAccount()
     logout.register_user("john_doe", "Password123!", "john@example.com", "john", "doe") 
     logout.login_user("john_doe", "Password123!")
     assert logout.logout_user("john_doe") == "User john_doe logged out."
 
 #logging out of account that isnt logged in
-def test_logout2():
+def test_logout21():
     logout = ClientAccount()
     logout.register_user("john_doe", "Password123!", "john@example.com", "john", "doe") 
     logout.login_user("john_doe", "Password123!")

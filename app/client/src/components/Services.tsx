a10fd56dcedd03488e1f5fc07133e1ca98481120
Cardoso0528 <cardosojuan938@gmail.com>
2025-04-15 10:39:51 -0500

    Update project files

---
+++ app/client/src/components/Services.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Services.css';
+
+const Services = () => {
+  return (
+    <div className="services-container">
+      <h1 className="services-title">Our Services</h1>
+      <div className="service-grid">
+        <div className="service-card">
+          <img src="images/service1.jpg" alt="Service 1" className="service-image" />
+          <h2 className="service-name">Haircut</h2>
+          <p className="service-description">Custom haircut by our expert stylists</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;

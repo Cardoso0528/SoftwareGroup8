e250f0840b167e43056a2dae058c911870b7edb4
Justin Ta <158676521+justinta1314@users.noreply.github.com>
2025-04-20 16:14:21 -0500

    Update accounts.ts

--- app/server/src/controllers/accounts.ts
+++ app/server/src/controllers/accounts.ts
@@ -1,1 +1,27 @@
+import express, { Request, Response, NextFunction } from 'express';
 
+import { getUsers, deleteUserById } from '../models/account.model';
+
+export const getAllAccounts = async (req: express.Request, res: express.Response, next: NextFunction) => {
+    try {
+        const users = await getUsers();
+
+        res.status(200).json(users);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+export const deleteAccount = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    try {
+        const { id } = req.params;
+
+        const deletedUser = await deleteUserById(id);
+
+        res.json(deletedUser);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}

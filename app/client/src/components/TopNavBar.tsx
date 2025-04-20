20abaeb7a00ceee38713aa7281845f539296368d
Kim Bui <ktb220000@utdallas.edu>
2025-04-20 16:21:18 -0500

    events for calendar

--- app/client/src/components/TopNavBar.tsx
+++ app/client/src/components/TopNavBar.tsx
@@ -11,11 +11,23 @@ import List from '@mui/material/List';
 import Divider from '@mui/material/Divider';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemButton from '@mui/material/ListItemButton';
+import { createTheme, ThemeProvider } from '@mui/material/styles'
 
 interface NavBarProps {
   navText: string; 
   drawerOptions: { label: string; path: string }[]; 
 }
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#ef69a7",
+      main: "#db2778",
+      dark: "#ae226b",
+      contrastText: "#fff",
+    },
+  },
+});
 
 export default function NavBar({ navText, drawerOptions }: NavBarProps) {
   const [drawerOpen, setDrawerOpen] = useState(false); 
@@ -31,48 +43,50 @@ export default function NavBar({ navText, drawerOptions }: NavBarProps) {
   };
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-            onClick={toggleDrawer(true)} 
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
-            {navText}
-          </Typography>
-        </Toolbar>
-      </AppBar>
+    <Box sx={{ flexGrow: 1}}>
+      <ThemeProvider theme={theme}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+              onClick={toggleDrawer(true)} 
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+              {navText}
+            </Typography>
+          </Toolbar>
+        </AppBar>
 
-      <Drawer
-        anchor="left" 
-        open={drawerOpen} 
-        onClose={toggleDrawer(false)} 
-      >
-        <Box
-          sx={{ width: 250 }}
-          role="presentation"
-          onClick={toggleDrawer(false)} 
-          onKeyDown={toggleDrawer(false)} 
+        <Drawer
+          anchor="left" 
+          open={drawerOpen} 
+          onClose={toggleDrawer(false)} 
         >
-          <List>
-            {drawerOptions.map((option, index) => (
-              <ListItemButton 
-                key={index}
-                onClick={() => handleNavigation(option.path)}>
-                <ListItemText primary={option.label} /> 
-              </ListItemButton>
-            ))}
-          </List>
-          <Divider />
-        </Box>
-      </Drawer>
+          <Box
+            sx={{ width: 250 }}
+            role="presentation"
+            onClick={toggleDrawer(false)} 
+            onKeyDown={toggleDrawer(false)} 
+          >
+            <List>
+              {drawerOptions.map((option, index) => (
+                <ListItemButton 
+                  key={index}
+                  onClick={() => handleNavigation(option.path)}>
+                  <ListItemText primary={option.label} /> 
+                </ListItemButton>
+              ))}
+            </List>
+            <Divider />
+          </Box>
+        </Drawer>
+      </ThemeProvider>
     </Box>
   );
 }
--- app/client/src/pages/HairstylistDashboard.tsx
+++ app/client/src/pages/HairstylistDashboard.tsx
@@ -5,19 +5,30 @@ import Calendar from '../components/Calendar'
 import NavBar from '../components/TopNavBar';
 import CardContent from '@mui/material/CardContent'
 import Typography from '@mui/material/Typography'
+import dayjs from 'dayjs'
 
 interface Appointment {
   title: string;
   services: string;
   date: string;
   time: string;
+  duration: number;
 }
 
 const appointments: Appointment[] = [
-  { title: "Appointment 1", services: "Haircut", date: "Thursday 3/13/25", time: "2:00 pm" },
-  { title: "Appointment 2", services: "Haircut", date: "Wednesday 3/26/25", time: "1:45 pm" },
-  { title: "Appointment 3", services: "Haircut", date: "Friday 4/11/25", time: "11:30 am" },
+  { title: "Appointment 1", services: "Haircut", date: "Thursday 3/13/25", time: "2:00 pm", duration: 60 },
+  { title: "Appointment 2", services: "Haircut", date: "Wednesday 3/26/25", time: "1:45 pm", duration: 45 },
+  { title: "Appointment 3", services: "Haircut", date: "Friday 4/11/25", time: "11:30 am", duration: 30 },
 ];
+
+const events = appointments.map((appointment) => {
+  const startDateTime = dayjs(`${appointment.date} ${appointment.time}`, "dddd MM/DD/YY h:mm a");
+  return {
+    title: appointment.title,
+    start: startDateTime.toDate(),
+    end: startDateTime.add(appointment.duration, 'minute').toDate(), 
+  };
+});
 
 export default function ClientDashboard() {
   return (
@@ -33,11 +44,13 @@ export default function ClientDashboard() {
       <Box display="flex" flexDirection={{ xs: "column", md: "row" }} gap={3} p={2}>
         <Box flex={1} display="flex" justifyContent="center" alignItems="center" 
         sx={{ height: { xs: "85vh", md: "95vh" }, flexGrow: 2}}>
-          <Calendar style={{
-            height: "100%",
-            width: "100%",
-            minHeight: "500px",
-            }}/>
+          <Calendar 
+            style={{
+              height: "100%",
+              width: "100%",
+              minHeight: "500px",
+            }}
+            events={events}/>
         </Box>
 
         <Box flex={1}>

535eb060a7adab1fec6c76d3ac1c779b927ccda8
Justin Ta <158676521+justinta1314@users.noreply.github.com>
2025-04-22 16:49:04 -0500

    Update authentication.ts

--- app/server/src/routes/authentication.ts
+++ app/server/src/routes/authentication.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import {login, register} from '../controllers/authentication';
+import {login, logout, register} from '../controllers/authentication';
 
 const router = express.Router();
 
 router.post('/login', login);
 router.post('/register', register);
+router.post('/logout', logout);
 
 export default router;
